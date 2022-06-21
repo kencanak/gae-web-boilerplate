e8f2bc7c532017eb33c5d1a3c3a51e0684c2ff4d
Kencana Kesuma <kesuma.kencana@yahoo.com>
2022-06-21 15:06:49 +0800

    fix(nunjucks-build): rebuild where there is file added/modified/deleted

--- development/webpack-plugins/nunjucks-build.ts
+++ development/webpack-plugins/nunjucks-build.ts
@@ -20,12 +20,56 @@ marked.setOptions({
 
 
 export default class NunjucksBuild {
+  files: Array<any> = [];
+  paths: Array<any> = [];
   // Any options should be passed in the constructor of your plugin,
   // (this is a public API of your plugin).
   constructor(options = {}) {
     // Applying user-specified options over the default options
     // and making merged options further available to the plugin methods.
     // You should probably validate all the options here as well.
+  }
+
+  _rebuildPage(compiler: any) {
+    // TODO: refactor this
+    // in order to make hot reload properly, i.e. should re-compile when component
+    // gets update, we need to use new nunjucks env
+    nunjucks.configure({
+      watch: false,
+      noCache: true,
+    });
+
+    const nunjucksEnv = new nunjucks.Environment(
+      new nunjucks.FileSystemLoader([`./src`, `./src/components`, `./src/pages`])
+    );
+    nunjucksEnv.addFilter('json', JSON.stringify);
+    nunjucksEnv.addFilter('markdown', marked.parse);
+
+    const pageHTML: Array<any> = getPageFiles(/.*\.njk$/);
+
+    pageHTML.forEach(async (page) => {
+      const out = path.join(page.name === 'home' ? '' : page.name, 'index.html');
+
+      const templateData = fs.readFileSync(page.entry, 'utf8');
+
+      let pageData = {};
+      const pageDataPath = path.join(__dirname, '..', SRC_FOLDER, 'data', `${page.name}.json`);
+      const globalDataPath = path.join(__dirname, '..', SRC_FOLDER, 'data', `global.json`);
+
+      if (fs.existsSync(pageDataPath)) {
+        pageData = JSON.parse(fs.readFileSync(pageDataPath, 'utf-8'));
+      }
+
+      let templateContent = nunjucksEnv.renderString(templateData, {
+        global: JSON.parse(fs.readFileSync(globalDataPath, 'utf-8')),
+        [page.name]: pageData,
+      });
+
+      compiler(
+        out,
+        templateContent,
+      );
+    });
   }
 
   // Define `apply` as its prototype method which is supplied with compiler as its argument
@@ -41,6 +85,16 @@ export default class NunjucksBuild {
     // RawSource is one of the "sources" classes that should be used
     // to represent asset sources in compilation.
     const { RawSource } = webpack.sources;
+
+    compiler.hooks.make.tapAsync(
+      'NunjucksBuildWebpackPlugin',
+      (compilation: any, callback: Function) => {
+        compilation.contextDependencies.add(path.resolve(__dirname, '..', SRC_FOLDER, 'components'));
+        compilation.contextDependencies.add(path.resolve(__dirname, '..', SRC_FOLDER, 'pages'));
+
+        callback();
+      }
+    );
 
     // add file to watch
     compiler.hooks.afterCompile.tapAsync(
@@ -62,9 +116,15 @@ export default class NunjucksBuild {
 
         const filesToWatch = [...pagesToWatch, ...componentToWatch];
 
-        filesToWatch.forEach((f: string) => {
-          compilation.fileDependencies.add(f);
+        filesToWatch.filter((f: string) => !this.files.includes(f)).forEach((f: string) => {
+          if (Array.isArray(compilation.fileDependencies)) {
+            compilation.fileDependencies.push(f);
+          } else {
+            compilation.fileDependencies.add(f);
+          }
         });
+
+        this.files = [...filesToWatch];
 
         callback();
       }
@@ -74,40 +134,7 @@ export default class NunjucksBuild {
     compiler.hooks.emit.tapAsync(
       'NunjucksBuildWebpackPlugin',
       (compilation: any, callback: Function) => {
-        // TODO: refactor this
-        // in order to make hot reload properly, i.e. should re-compile when component
-        // gets update, we need to use new nunjucks env
-        nunjucks.configure({
-          watch: false,
-          noCache: true,
-        });
-
-        const nunjucksEnv = new nunjucks.Environment(
-          new nunjucks.FileSystemLoader([`./src`, `./src/components`, `./src/pages`])
-        );
-        nunjucksEnv.addFilter('json', JSON.stringify);
-        nunjucksEnv.addFilter('markdown', marked.parse);
-
-        const pageHTML: Array<any> = getPageFiles(/.*\.njk$/);
-
-        pageHTML.forEach(async (page) => {
-          const out = path.join(page.name === 'home' ? '' : page.name, 'index.html');
-
-          const templateData = fs.readFileSync(page.entry, 'utf8');
-
-          let pageData = {};
-          const pageDataPath = path.join(__dirname, '..', SRC_FOLDER, 'data', `${page.name}.json`);
-          const globalDataPath = path.join(__dirname, '..', SRC_FOLDER, 'data', `global.json`);
-
-          if (fs.existsSync(pageDataPath)) {
-            pageData = JSON.parse(fs.readFileSync(pageDataPath, 'utf-8'));
-          }
-
-          let templateContent = nunjucksEnv.renderString(templateData, {
-            global: JSON.parse(fs.readFileSync(globalDataPath, 'utf-8')),
-            [page.name]: pageData,
-          });
-
+        this._rebuildPage((out: string, templateContent: string) => {
           compilation.emitAsset(
             out,
             new RawSource(templateContent)
