d4ac500b4c1cbf444f269a10553c141d2c277bd2
Kencana Kesuma <kesuma.kencana@yahoo.com>
2022-10-11 15:37:45 +0800

    add(utils): add useful utils, pubsub + debounce

---
+++ development/src/utils/utils.ts
@@ -0,0 +1,17 @@
+const debounce = <F extends (...args: Parameters<F>) => ReturnType<F>>(
+  func: F,
+  waitFor: number,
+) => {
+  let timeout: NodeJS.Timeout;
+
+  const debounced = (...args: Parameters<F>) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), waitFor);
+  }
+
+  return debounced;
+};
+
+export {
+  debounce,
+};
